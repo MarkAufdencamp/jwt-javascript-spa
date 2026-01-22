5c2dee6995359551832cd712170eadddea37d5bb
Mark Aufdencamp <markaufdencamp@Kimberlys-Mac-mini.local>
2026-01-22 17:11:03 -0500

    Stored the JWT in localStorage. Roll to Main Menu page.

--- index.js
+++ index.js
@@ -29,8 +29,10 @@ loginForm.addEventListener('submit', async (e) => {
     // On success, you'd typically save the token and redirect
     // For this example, we'll just display a success message
     console.log('Received token:', data.token);
+    localStorage.setItem("jwt", data.token);
     message.style.color = 'green';
     message.textContent = 'Login successful! Token is in the console.';
+	  window.open("mainmenu.html", "_self");
     
   } catch (error) {
     message.style.color = 'red';
